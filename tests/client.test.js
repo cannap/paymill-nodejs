760aa9d764fe0c5d40d81b8eadc1c330f9a5319e
cannap <sh4fty@gmail.com>
2017-09-25 01:10:23 +0200

    fix client details ttest

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,3 +1,4 @@
+'use strict'
 import test from 'ava'
 import { gateway } from './shared'
 import Chance from 'chance'
@@ -5,7 +6,6 @@ const chance = new Chance()
 
 test('create', async t => {
   const email = chance.email()
-
   const client = await gateway.clients.create({
     email,
     description: 'Some description'
@@ -34,7 +34,10 @@ test('details', async t => {
     email: email,
     description: 'Some description'
   })
-  t.is(client.email, email)
+
+  const clientDetails = await gateway.clients.details(client.id)
+
+  t.is(clientDetails.email, email)
 })
 
 test('delete', async t => {
